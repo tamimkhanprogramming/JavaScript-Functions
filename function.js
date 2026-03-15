36d9c1312cacdcf47d92fe1ba71af02531e9c77a
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-15 15:09:31 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,23 @@
+const number = 45;
+
+function fanOffKor(){
+
+    console.log('Bosha Theke Uthe Dara');
+    console.log('Walk Toward The Switch');
+    console.log('Click The Switch to Off The Fan');
+
+}
+// function name(){
+// }
+function brushTeeth(){
+
+    console.log('Pick Up The Brush');
+    console.log('Add Paste');
+    console.log('2 min goso ar majho');
+
+}
+
+// Call The Function
+fanOffKor();
+console.log('---------');
+brushTeeth();
