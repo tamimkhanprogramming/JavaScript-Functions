3e4bff8c2ca99ce4adf6931d0490eb73a5255d06
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-15 23:32:39 +0600

    Updated

---
+++ parameter.js
@@ -0,0 +1,42 @@
+function vaatKhao(){
+
+    console.log('hand Wash Kore aso');
+    console.log('boso');
+    console.log('plate e khabar dhabar nao');
+    console.log('gopat gopat kore khao');
+
+}
+// vaatKhao();
+
+function square(number){
+
+    console.log('Value of The Number Parameter',number);
+    const borgo = number*number;
+    console.log('Square of the given number is: ',borgo);
+
+
+}
+
+square(4);
+console.log('------');
+square(5);
+console.log('------');
+square(12);
+console.log('------');
+square(405);
+
+function add(num1,num2){
+
+    const sum = num1 + num2;
+    console.log('Sum Of The Number is: ',sum);
+}
+
+add(5,71);
+add(65,31);
+
+function addAll(a,b,c,d,e){
+
+    const sum = a+b+c+d+e;
+    console.log("Sum of The All Numbers Are: ",sum);
+}
+addAll(10,20,30,40,50);
