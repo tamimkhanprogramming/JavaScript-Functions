722fb5db657fca6272a833c2bd0999149fb24d70
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-15 23:40:07 +0600

    Updated

--- parameter.js
+++ parameter.js
@@ -1,42 +1,63 @@
-function vaatKhao(){
+// function vaatKhao(){
 
-    console.log('hand Wash Kore aso');
-    console.log('boso');
-    console.log('plate e khabar dhabar nao');
-    console.log('gopat gopat kore khao');
+//     console.log('hand Wash Kore aso');
+//     console.log('boso');
+//     console.log('plate e khabar dhabar nao');
+//     console.log('gopat gopat kore khao');
 
-}
-// vaatKhao();
+// }
+// // vaatKhao();
+
+// function square(number){
+
+//     console.log('Value of The Number Parameter',number);
+//     const borgo = number*number;
+//     console.log('Square of the given number is: ',borgo);
+
+
+// }
+
+// square(4);
+// console.log('------');
+// square(5);
+// console.log('------');
+// square(12);
+// console.log('------');
+// square(405);
+
+// function add(num1,num2){
+
+//     const sum = num1 + num2;
+//     console.log('Sum Of The Number is: ',sum);
+// }
+
+// add(5,71);
+// add(65,31);
+
+// function addAll(a,b,c,d,e){
+
+//     const sum = a+b+c+d+e;
+//     console.log("Sum of The All Numbers Are: ",sum);
+// }
+// addAll(10,20,30,40,50);
 
 function square(number){
 
-    console.log('Value of The Number Parameter',number);
-    const borgo = number*number;
-    console.log('Square of the given number is: ',borgo);
+    const square_num = number*number;
 
-
+    console.log("Square Of The Numbers Are: ",square_num);
 }
 
 square(4);
-console.log('------');
-square(5);
-console.log('------');
-square(12);
-console.log('------');
-square(405);
+console.log("------");
+square(8);
+console.log("------");
+square(16);
 
-function add(num1,num2){
+function sum(num1,num2){
 
-    const sum = num1 + num2;
-    console.log('Sum Of The Number is: ',sum);
+    const sum_num = num1 + num2;
+    console.log("Sum of The Two Numbers Are: ",sum_num);
 }
 
-add(5,71);
-add(65,31);
-
-function addAll(a,b,c,d,e){
-
-    const sum = a+b+c+d+e;
-    console.log("Sum of The All Numbers Are: ",sum);
-}
-addAll(10,20,30,40,50);
+sum(10,20);
