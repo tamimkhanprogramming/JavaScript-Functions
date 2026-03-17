07e5b7d226a8f62dd05ec7ba29b43d582551105f
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-17 13:17:00 +0600

    New

--- arguments.js
+++ arguments.js
@@ -14,7 +14,7 @@ doubleIt(873);
 const number = 55;
 doubleIt(number);
 
-const money = 112;
+const money_ = 112;
 doubleIt(money);
 
 function difference(num1,num2){
@@ -23,9 +23,39 @@ function difference(num1,num2){
     console.log(num1,num2,'Difference',diff_num);
 }
 
+const father_age_ = 63;
+
+const son_age_ = 27;
+
+difference(father_age,son_age);
+
+// Practice by Myself
+
+function number_Multiply(number){
+
+    const Multiply_Num = number * 2;
+    console.log("Multiply By 2 is: ",Multiply_Num);
+}
+
+number_Multiply(10);
+const money = 100;
+number_Multiply(money);
+
+function difference(num1,num2){
+
+    const diff_num_Add = num1 - num2;
+
+    console.log("Difference of Two Given Numbers Are: ",diff_num_Add);
+}
+
 const father_age = 63;
 
 const son_age = 27;
 
+const mother_age = 53;
+
+const son_age_M = 27;
 difference(father_age,son_age);
+difference(mother_age,son_age_M);
+
 
---
+++ return.js
@@ -0,0 +1,25 @@
+function tenTimes(number){
+
+    const result = number * 10;
+    return result;
+}
+
+const output = tenTimes(5);
+
+console.log("output",output);
+
+function cutHalf(number){
+
+    const half = number/2;
+
+    return half;
+}
+
+const cut_Half = cutHalf(10);
+
+console.log("CutHalf: ",cut_Half);
+
+const bignumber = tenTimes(99);
+
+console.log("BigNumber: ",bignumber);
+
