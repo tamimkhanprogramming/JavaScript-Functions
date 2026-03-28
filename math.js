4e8967ca38b53757dc800580854f4eeb22728cb7
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-28 22:00:33 +0600

    Updated

---
+++ math.js
@@ -0,0 +1,18 @@
+ const min = Math.min(45,21,65,99,126)
+
+ console.log(min);
+
+ const max = Math.max(45,21,65,99,126)
+
+ console.log(max);
+ console.log(Math.PI);
+ console.log(Math.abs(10-5));
+ console.log(Math.round(2.23));
+ console.log(Math.round(2.85));
+ console.log(Math.floor(2.9995));
+ console.log(Math.ceil(2.85));
+ console.log(Math.random());
+ console.log(Math.random()*10);
+ const rand = Math.round(Math.random()*10)
+
+ console.log(rand)
