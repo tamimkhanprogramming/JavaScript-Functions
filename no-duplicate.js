744655c53e0d62d356391b0c9b6ae8fa46cb0ec3
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-03-28 12:13:58 +0600

    Updated

--- no-duplicate.js
+++ no-duplicate.js
@@ -1,15 +1,17 @@
 const biryanikhor = ['abul','babul','cabul','abul','babul','dabul'];
 
-const numbers = [1,5,61,5,87,7,5,81,61];
 
 function noDuplicate(array){
-   const unique = [];
-  for(const item of array){
-          if(unique.includes(item)===false) {
-            unique.push(item);
-          }
-  }
 
+  const unique = [];
+
+  for(const item of array){
+
+    if(unique.includes(item)===false){
+
+      unique.push(item);
+    }
+  }
   return unique;
 }
 
