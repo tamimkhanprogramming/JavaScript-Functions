f4ee89a66460813a9c8c295ca35a721029b8a018
Tamim Rahman Khan <tamimkhanbdworks@gmail.com>
2026-04-04 12:53:30 +0600

    Updated

--- min.js
+++ min.js
@@ -2,8 +2,17 @@ const prices = [20000,160000,50000,100000,12000,30000,35000];
 
 
 function getMin(numbers){
+    
+    let min = numbers[0];
+    for(const num of numbers){
+        if(num < min){
 
-    console.log(numbers);
+            min = num;
+        }
+        
+    }
+
+    return min;
 }
 
 const cheap = getMin(prices);
